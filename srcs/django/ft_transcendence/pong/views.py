93364cc3c0285207935e30b3d7a9914f620194df
Arsene <a.rurangiza@gmail.com>
2024-04-26 14:14:54 +0200

    [FIX] spa: AJAX naviguation to-from game and chat

--- srcs/django/ft_transcendence/pong/views.py
+++ srcs/django/ft_transcendence/pong/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from render_block import render_block_to_string
+from django.http import HttpResponse
 
 # Create your views here.
 def pong_game(request):
@@ -8,4 +10,8 @@ def play(request):
     return render(request, 'play.html')
 
 def quick_play(request):
+    if 'HTTP_HX_REQUEST' in request.META:
+        context = {"request": request}
+        html = render_block_to_string('quickplay.html', 'body', context)
+        return HttpResponse(html)
     return render(request, 'quickplay.html')
